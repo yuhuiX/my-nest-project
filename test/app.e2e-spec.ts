093d5ce8f9c26c6027aebf2d17dc527457a9ff42
Yuhui Xiong <yuhui.xiong.2018@gmail.com>
2020-07-14 11:55:33 +0200

    fix e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,8 @@
 import * as request from 'supertest';
-import { Test } from '@nestjs/testing';
+
 import { AppModule } from './../src/app.module';
 import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -19,6 +20,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Hello Master!');
   });
 });
